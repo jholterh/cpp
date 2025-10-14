63389f2b81e611a798d1d17239d73d07c15b62cd
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-10-14 14:10:43 +0200

    feat: Bureaucrat header file done

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -8,13 +8,42 @@
 
 class Bureaucrat
 {
-    Bureaucrat();
-    Bureaucrat(std::string &name, int grade);
-    Bureaucrat(const Bureaucrat &copy);
-    Bureaucrat &operator=(const Bureaucrat &other);
-    ~Bureaucrat();
+    public:
 
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                const char* what() const noexcept override
+                {
+                    return "Grade too high!";
+                }
+        };
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                const char* what() const noexcept override
+                {
+                    return "Grade too low!";
+                }
+        };
+
+        Bureaucrat();
+        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const Bureaucrat &copy);
+        Bureaucrat &operator=(const Bureaucrat &other);
+        ~Bureaucrat();  
+
+        std::string getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+        
+    private:
+        const std::string   name;
+        int                 grade;
 };
 
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
 
  #endif
