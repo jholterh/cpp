3d64eb733836eee38523b279464621c0bde31d89
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-10-17 18:17:48 +0200

    feat: added first form header file

---
+++ ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
+#include <string>
+#include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
+{
+    public:
+        ShrubberyCreationForm();
+        ShrubberyCreationForm(std::string &target);
+        ShrubberyCreationForm(const ShrubberyCreationForm &copy);
+        ShrubberyCreationForm &operator=(const ShrubberyCreationForm &other);
+        
+    protected:
+        virtual void action() const;
+
+    private:
+        std::string target; 
+};
+
+#endif
